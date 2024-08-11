4596fb0cd31cb76da1e10f1a242c58754bbe8ebb
mitu2 <chenmoand@outlook.com>
2024-08-11 19:51:10 +0800

    chore: 将前端项目配置子Git, 修改环境配置文件

---
+++ app/src/main/java/vip/hoode/web/IndexController.java
@@ -0,0 +1,12 @@
+package vip.hoode.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author mitu2
+ */
+@Controller
+@RequestMapping
+public class IndexController {
+}
